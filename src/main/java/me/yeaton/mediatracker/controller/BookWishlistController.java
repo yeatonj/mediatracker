532b70574c3cf63548b0ca1f3ba7fdba9fd0ba24
Josh Yeaton <yeaton.josh@gmail.com>
2025-08-17 21:09:11 -0400

    Create book wishlist controller

--- src/main/java/me/yeaton/mediatracker/controller/BookWishlistController.java
+++ src/main/java/me/yeaton/mediatracker/controller/BookWishlistController.java
@@ -12,43 +12,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import me.yeaton.mediatracker.model.BookRead;
-import me.yeaton.mediatracker.service.BookReadService;
-import me.yeaton.mediatracker.service.BookReadService.SerializedBookRead;
-
+import me.yeaton.mediatracker.model.BookWishlist;
+import me.yeaton.mediatracker.service.BookWishlistService;
+import me.yeaton.mediatracker.service.BookWishlistService.SerializedBookWishlist;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping("api/books-read")
-public class BookReadController {
+@RequestMapping("/api/books-wishlist")
+public class BookWishlistController {
 
     @Autowired
-    private BookReadService bookReadService;
+    private BookWishlistService bookWishlistService;
 
     // Create
     @PostMapping
-    public BookRead createBookRead(@RequestBody SerializedBookRead serializedBookRead) {
-        return bookReadService.createBookRead(serializedBookRead);
+    public BookWishlist createBookWishlist(@RequestBody SerializedBookWishlist serializedBookWishlist) {
+        return bookWishlistService.createBookWishlist(serializedBookWishlist);
     }
 
     // Read
     @GetMapping
-    public Iterable<BookRead> fetchBooksRead() {
-        return bookReadService.fetchBookRead();
+    public Iterable<BookWishlist> fetchBookWishlists() {
+        return bookWishlistService.fetchBookWishlist();
     }
 
     // Update
     @PutMapping("/{id}")
-    public BookRead updateBookRead(@PathVariable("id") UUID id, @RequestBody SerializedBookRead serializedBookRead) {
-        return bookReadService.updateBookRead(serializedBookRead, id);
+    public BookWishlist updateBookWishlist(@PathVariable("id") UUID id, @RequestBody SerializedBookWishlist serializedBookWishlist) {
+        return bookWishlistService.updateBookWishlist(serializedBookWishlist, id);
     }
 
     // Delete
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteBookRead(@PathVariable("id") UUID id) {
-        bookReadService.deleteBookRead(id);
+    public ResponseEntity<Void> deleteBookWishlist(@PathVariable("id") UUID id) {
+        bookWishlistService.deleteBookWishlist(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
